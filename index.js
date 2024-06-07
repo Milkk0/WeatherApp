f48554d44bef11cfb70f675f230fc76901744948
Milkk0 <milkkodev010@gmail.com>
2024-06-07 15:59:01 +0000

    initalize boilerplate

---
+++ index.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container')
+const inputForm = document.querySelector('.inputForm')
+const inputBox = document.querySelector('.inputBox')
+const submitBtn = document.querySelector('.submitBtn')
+const apiKey = ''
+let city
+const apiUrl = ''
